b1dbdb6ed33b9b141e21c57c484741c4fb847135
Acastrog-13 <161310060+Acastrog-13@users.noreply.github.com>
2025-12-24 18:05:15 +0100

    Creación del proyecto, de la estructura de la pieza y las funciones relacionadas

---
+++ Tetris/Core/Inc/macros.h
@@ -0,0 +1,22 @@
+#ifndef MACROS_H
+#define MACROS_H
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define TABLERO_ALTO 20      // Tablero lógico (memoria)
+#define TABLERO_ANCHO 8      // Ancho estándar
+#define VISIBLE_ALTO 16      // Lo que realmente muestra la matriz LED
+#define SPAWN_ZONA (TABLERO_ALTO - VISIBLE_ALTO) // Las 4 filas de arriba
+
+// Definición de direcciones para el Joystick y movimientos
+typedef enum {
+    NINGUNA = 0,
+    ARRIBA,
+    ABAJO,
+    IZQUIERDA,
+    DERECHA
+} Direccion_t;
+
+#endif // MACROS_H
---
+++ Tetris/Core/Inc/pieza.h
@@ -0,0 +1,34 @@
+#ifndef PIEZA_H
+#define PIEZA_H
+
+#include "macros.h"
+
+#define PIEZA_ALTO 4  //alto de la matriz que contiene a la pieza
+#define PIEZA_ANCHO 4 //ancho de la matriz que contiene a la pieza
+#define MAX_PIEZAS 7  //número máximo de piezas
+
+typedef struct {
+    uint8_t x;              // Posición X en el tablero
+    uint8_t y;              // Posición Y en el tablero
+    uint8_t tipo;           // Identificador de la forma
+    uint8_t rotacion;       // Estado de rotación
+    uint8_t forma[PIEZA_ALTO][PIEZA_ANCHO]; //contiene la rotación actual de las piezas
+} Pieza_t;
+
+
+//Atualiza la forma de la pieza al rotar
+void setForma(Pieza_t* me);
+
+// Inicializa una pieza de tipo aleatorio en una nueva en la parte superior con un tipo aleatorio
+void Pieza_Init(Pieza_t *me);
+
+// Cambia la matriz interna 'forma' según la rotación siguiente
+void Pieza_Rotar(Pieza_t *me);
+
+// Mueve las coordenadas de la pieza
+void Pieza_Mover(Pieza_t *me, Direccion_t dir);
+
+// Retorna true si en esa coordenada relativa de la pieza (i, j) hay un bloque
+bool Pieza_GetBloque(Pieza_t *me, uint8_t i, uint8_t j);
+
+#endif // PIEZA_H
